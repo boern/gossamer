7d67adf349c9638a9942558e59fa152e55942965
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-09-13 21:19:09 +0000

    Split full and archive node impl in their own file

---
+++ internal/pruner/archive.go
@@ -0,0 +1,14 @@
+// Copyright 2022 ChainSafe Systems (ON)
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package pruner
+
+import "github.com/ChainSafe/gossamer/lib/common"
+
+// ArchiveNode is a no-op since we don't prune nodes in archive mode.
+type ArchiveNode struct{}
+
+// StoreJournalRecord for archive node doesn't do anything.
+func (*ArchiveNode) StoreJournalRecord(_, _ map[string]struct{}, _ common.Hash, _ int64) (_ error) {
+	return nil
+}
